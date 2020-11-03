625b67e3a7611139b66579eb0b2cf674123413bb
richardlauren <48044898+richardlauren@users.noreply.github.com>
2020-11-03 16:23:25 +0700

    update
    detail fragment, home fragment

--- app/src/main/java/com/example/ParkirYuk/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ParkirYuk/ui/home/HomeFragment.java
@@ -1,18 +1,34 @@
 package com.example.ParkirYuk.ui.home;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.ParkirYuk.DetailsFragment;
+import com.example.ParkirYuk.HomeActivity;
 import com.example.ParkirYuk.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,26 +37,97 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
     private static final String TAG = "tag";
     private HomeViewModel homeViewModel;
+    TextView textView;
+    ArrayList<String> arrayList;
+    Dialog dialog;
+    Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        //homeViewModel =
+        //        ViewModelProviders.of(this).get(HomeViewModel.class);
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
+        //final TextView textView = v.findViewById(R.id.text_view);
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        // @Override
+        //public void onChanged(@Nullable String s) {
+        //   textView.setText(s);
+        //}
+        //});
 
-        final TextView textView = v.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        textView = v.findViewById(R.id.text_view);
+        data();
+
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.dialog_searchable_spinner);
+                dialog.getWindow().setLayout(1000,1500);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+                EditText editText = dialog.findViewById(R.id.edit_text);
+                ListView listView = dialog.findViewById(R.id.list_view);
+
+                final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                        android.R.layout.simple_list_item_1, arrayList);
+
+                listView.setAdapter(adapter);
+
+                editText.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        adapter.getFilter().filter(charSequence);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                        dialog.dismiss();
+                    }
+
+                });
+
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getContext(), "Ini adalah contoh Toast di Android",Toast.LENGTH_LONG).show();
+                    }
+                });
+
             }
         });
 
-
         return v;
     }
+
+    public void data(){
+        arrayList = new ArrayList<>();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+    }
+
+
 }
